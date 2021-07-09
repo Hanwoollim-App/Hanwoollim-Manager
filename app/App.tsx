cc211467932a6d0b3e2759e8646669ef7e0e5358
Kim Dong Hyun <akainoo123@gmail.com>
2021-07-09 17:38:27 +0900

    feat: add vscode setting

--- app/App.tsx
+++ app/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import {Text, View} from 'react-native';
 
-
-function App(){
+function App() {
   return (
     <View>
       <Text>Hello World!</Text>
     </View>
-  )
+  );
 }
 
 export default App;
