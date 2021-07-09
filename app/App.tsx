e0e18c2f295099d5bc4d6c9e6d7c217b3f9876e5
Kim Dong Hyun <akainoo123@gmail.com>
2021-07-09 17:34:32 +0900

    feat: write the App.tsx file

---
+++ app/App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(){
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  )
+}
+
+export default App;
