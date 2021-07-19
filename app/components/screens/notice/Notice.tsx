779e79ba3423a3c02ec5b3906a42cae343b4092b
Aperture507 <williamzz@naver.com>
2021-07-19 19:26:50 +0900

    feat: 공지사항 검색창 추가

--- app/components/screens/notice/Notice.tsx
+++ app/components/screens/notice/Notice.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Image} from 'react-native';
 import {
   NavigationProp,
   ParamListBase,
@@ -10,10 +10,39 @@ import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
 import CustomHeader from '../../common/CustomHeader';
 import HEADER_TITLE from '../../../utils/constant/naviagation/NavigationheaderUtils';
 import CustomStatusBar from '../../common/CustomStatusBar';
+import {
+  fontPercentage,
+  heightPercentage,
+  widthPercentage,
+} from '../../../utils/constant/common/design/Responsive';
+import searchIcon from '../../../assets/images';
 
-const sytles = StyleSheet.create({
+const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  searchTextInput: {
+    width: widthPercentage(335),
+    height: heightPercentage(58),
+    marginTop: heightPercentage(20),
+    marginLeft: widthPercentage(20),
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderRadius: widthPercentage(10),
+    backgroundColor: 'white',
+    fontSize: fontPercentage(20),
+    fontFamily: 'NotoSansKR-Bold',
+    paddingLeft: 13,
+  },
+  ImageStyle: {
+    width: widthPercentage(25),
+    height: heightPercentage(25),
+    marginTop: heightPercentage(37),
+    marginLeft: widthPercentage(-40),
+  },
+  SectionStyle: {
+    flexDirection: 'row',
   },
 });
 
@@ -26,14 +55,24 @@ function Notice() {
   return (
     <>
       <CustomStatusBar />
-      <View style={sytles.root}>
+      <View style={styles.root}>
         <CustomHeader
           headerTitle={HEADER_TITLE.Notice}
           headerLeft={true}
           leftIcon={icon}
           leftIconClickListner={navigation.goBack}
         />
-        <Text>{'공지사항 화면입니다.'}</Text>
+        <View style={styles.SectionStyle}>
+          <TextInput
+            style={styles.searchTextInput}
+            placeholder="검색"
+            placeholderTextColor="#a2a2a2"
+          />
+          <Image
+            source={require('../../../assets/images/searchIcon.png')}
+            style={styles.ImageStyle}
+          />
+        </View>
       </View>
     </>
   );
