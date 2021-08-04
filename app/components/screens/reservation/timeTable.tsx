d73285d32cde24cd5e1a7ebff91a8e114b909e4a
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-08-05 02:27:18 +0900

    feat: 예약박스 색 생성 함수

--- app/components/screens/reservation/timeTable.tsx
+++ app/components/screens/reservation/timeTable.tsx
@@ -116,6 +116,49 @@ function TimeTable() {
     [], // 토
     [], // 일
   ];
+
+  const colorGenerator = (num: number) => {
+    const colorList = [
+      'rgba(246,206,218,1)',
+      'rgba(250,227,209,1)',
+      'rgba(248,238,207,1)',
+      'rgba(224,245,214,1)',
+      'rgba(215,235,252,1)',
+      'rgba(235,217,244,1)',
+      'rgba(228,223,217,1)',
+      'rgba(212,196,251,1)',
+      'rgba(193,225,197,1)',
+      'rgba(190,211,243,1)',
+      '#81E1B8',
+      'rgba(190,218,220,1)',
+      'rgba(254,243,189,1)',
+      'rgba(247,141,167,1)',
+      'rgba(196,222,246,1)',
+      'rgba(250,208,195,1)',
+      '#cc9af4',
+      '#f8b3eb',
+      '#ff8080',
+      '#9af49f',
+      '#9aeff4',
+      '#a8e6cf',
+      '#fdffab',
+      'rgba(0,208,132,0.9)',
+      'rgba(217,227,240,0.9)',
+      'rgba(105,108,137,0.8)',
+      'rgba(142,209,252,0.9)',
+      'rgba(6,147,227,0.8)',
+      'rgba(153,0,239,0.9)',
+      'rgba(235,20,76,0.9)',
+      'rgba(83,0,235,1)',
+      'rgba(18,115,222,1)',
+      'rgba(0,107,118,1)',
+      'rgba(129,199,132,1)',
+      'rgba(184,0,0,1)',
+    ];
+
+    return colorList[num % colorList.length];
+  };
+
   function xPosGenerator(day: number): number {
     return widthPercentage(46 * day) + 14;
   }
