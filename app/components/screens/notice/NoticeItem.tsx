1b58f350b8f2ecf986b4931dad181c61cae5113f
Aperture507 <williamzz@naver.com>
2021-08-13 13:44:59 +0900

    fix: react-native-dropdown-picker 추가

--- app/components/screens/notice/NoticeItem.tsx
+++ app/components/screens/notice/NoticeItem.tsx
@@ -43,7 +43,7 @@ function NoticeItem({title, date}: NoticeItemInterface) {
   const navigation: NavigationProp<ParamListBase> = useNavigation();
 
   const NoticeClickListener = () => {
-    navigation.navigate('NoticeNavigator', {screen: 'NoticeDetail'});
+    navigation.navigate('', {screen: 'NoticeDetail'});
   };
 
   return (
