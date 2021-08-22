4b0202642f82c8e670ae6a751d91322167d5728b
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-08-22 13:32:39 +0900

    feat/ lint error

--- app/components/screens/signIn/index.tsx
+++ app/components/screens/signIn/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import {
   NavigationProp,
   ParamListBase,
